b5e7d3b181bb72c5a2b87f9f70fd94e8e6c0a42a
marcosvalter <m.valter@ua.pt>
2014-05-11 00:45:04 +0100

    Update MatrixDisplay.cpp

--- MatrixDisplay.cpp
+++ MatrixDisplay.cpp
@@ -18,30 +18,6 @@
 #include "Arduino.h"
 
 // MAX7219 pins
-int CSPin; // CS pin
-int CLKPin; // CLK pin
-int DINPin; // DIN pin
-
-/*/*
- * This module is part of a small programming project just for fun
- * You may freely use and modify this code, NO WARRANTY, blah blah blah,
- * as long as you give proper credit to the original and subsequent authors.
- *
- * Date:        Author:
- * 2014-04-09   Marcos Valter Vidal Russo Alegre <m.valter@ua.pt>
- *
- */
-
-/*
- * This module drives a dual MAX7219 Led Matrix Controller
- * Please read MAXIM-7219 Manual for further information
- * and OPCode settings.
- */
-
-#include "MatrixDisplay.h"
-#include "Arduino.h"
-
-// MAX7219 pins
 int CSPin;      // CS pin
 int CLKPin;     // CLK pin
 int DINPin;     // DIN pin
